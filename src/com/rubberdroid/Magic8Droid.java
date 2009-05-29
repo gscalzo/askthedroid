361c6ac6d98a57420fd29adcea801f21d20fd3e5
J.Ordan <giordano.scalzo@rubberduck.it>
2009-05-30 00:17:04 +0200

    bare bone implementation, without sound, animation and shake detector

--- src/com/rubberdroid/Magic8Droid.java
+++ src/com/rubberdroid/Magic8Droid.java
@@ -1,13 +1,49 @@
 package com.rubberdroid;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class Magic8Droid extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class Magic8Droid extends Activity implements OnClickListener {
+	private Random rand = new Random();
+	private String msgs[] = { "Are you kidding?", "As I see it, yes.",
+			"Ask again later.", "Better not tell you now.",
+			"Cannot predict now.", "Concentrate and ask again.",
+			"Definitely not.", "Don't bet on it.", "Don't count on it.",
+			"Forget about it. ", "Go for it!", "I have my doubts.",
+			"It is certain.", "It is decidedly so.", "Looking good!",
+			"Looks good to me!", "Most likely.", "My reply is no.",
+			"My sources say no.", "Outlook good.", "Outlook not so good.",
+			"Outlook so so.", "Probably.", "Reply hazy, try again.",
+			"Signs point to yes.", "Very doubtful.", "Who knows?",
+			"Without a doubt.", "Yes.", "Yes - definitely.",
+			"Yes, in due time.", "You may rely on it.",
+			"You will have to wait.", };
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		View button = findViewById(R.id.ask);
+		button.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		String msg = randomMsgù();
+		Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+		toast.setGravity(Gravity.CENTER, 0, 0);
+		toast.show();
+	}
+
+	private String randomMsgù() {
+		int idx = rand.nextInt(msgs.length);
+		String msg = msgs[idx];
+		return msg;
+	}
 }
